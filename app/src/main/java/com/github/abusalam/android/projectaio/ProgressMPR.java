f4c4d375060740b5665f0ee2b581c16e2987327b
abusalam <abusalamparvezalam@gmail.com>
2015-03-01 18:20:29 +0530

    Working on MPR Data Binding

--- app/src/main/java/com/github/abusalam/android/projectaio/ProgressMPR.java
+++ app/src/main/java/com/github/abusalam/android/projectaio/ProgressMPR.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -30,6 +33,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class ProgressMPR extends ActionBarActivity {
@@ -40,21 +44,22 @@ public class ProgressMPR extends ActionBarActivity {
      * This is to prevent the user from generating too many HOTP codes in a short period of time.
      */
     private static final long HOTP_MIN_TIME_INTERVAL_BETWEEN_CODES = 5000;
-    /**
-     * The maximum amount of time (milliseconds) for which a HOTP code is displayed after it's been
-     * generated.
-     */
-    private static final long HOTP_DISPLAY_TIMEOUT = 2 * 60 * 1000;
+
+    static final String API_URL = "http://10.42.0.1/apps/mpr/AndroidAPI.php";
+
     protected PinInfo mUser;
     private JSONArray respJsonArray;
     private RequestQueue rQueue;
     private AccountDb mAccountDb;
     private OtpSource mOtpProvider;
 
+    private HashMap<String, String> mScheme;
+
     private Spinner spnSchemes;
     private Spinner spnWorks;
     private EditText etExpAmount;
     private EditText etRemarks;
+    private Button btnSave;
 
     private int mSelectedItemIndex;
 
@@ -76,8 +81,14 @@ public class ProgressMPR extends ActionBarActivity {
         spnWorks = (Spinner) findViewById(R.id.spnWorks);
         etExpAmount = (EditText) findViewById(R.id.etExpAmount);
         etRemarks = (EditText) findViewById(R.id.etRemarks);
+        btnSave = (Button) findViewById(R.id.btnSave);
+
+        mScheme = new HashMap<String, String>();
 
         getUserSchemes();
+
+        spnSchemes.setOnItemSelectedListener(new SchemeSelectionListener());
+        btnSave.setOnClickListener(new UpdateClickListener());
     }
 
     @Override
@@ -125,49 +136,41 @@ public class ProgressMPR extends ActionBarActivity {
         private boolean hotpCodeGenerationAllowed = true;
     }
 
-
     private void getUserSchemes() {
 
         final JSONObject jsonPost = new JSONObject();
 
-        try {
-            mUser.pin = mOtpProvider.getNextCode(mUser.user);
-            mUser.hotpCodeGenerationAllowed = false;
-        } catch (OtpSourceException e) {
-            Toast.makeText(getApplicationContext(), "Error: " + e.getMessage()
-                    + " MDN:" + mUser.user, Toast.LENGTH_SHORT).show();
-            return;
-        }
+        Log.e("P-Counter: ", "" + mAccountDb.getCounter(mUser.user));
 
         try {
             jsonPost.put("API", "US");
-            jsonPost.put("MDN", mUser.user);
-            jsonPost.put("UID", "1"); // TODO Supply Dynamic UserMapID instead of Static
-            jsonPost.put("OTP", mUser.pin);
+            jsonPost.put("UID", "5"); // TODO Supply Dynamic UserMapID instead of Static
         } catch (JSONException e) {
             e.printStackTrace();
             return;
         }
 
         JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
-                DashAIO.API_URL, jsonPost,
+                API_URL, jsonPost,
                 new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.d(TAG, "UserSchemes: " + response.toString());
+                        Log.e(TAG, "UserSchemes: " + response.toString());
                         Toast.makeText(getApplicationContext(), response.optString(DashAIO.KEY_STATUS), Toast.LENGTH_SHORT).show();
                         try {
                             respJsonArray = response.getJSONArray("DB");
-                            ArrayList<String> groupList = new ArrayList<String>();
+                            ArrayList<String> SchemeList = new ArrayList<String>();
 
                             for (int i = 0; i < respJsonArray.length(); i++) {
-                                groupList.add(respJsonArray.getJSONObject(i).optString("GroupName"));
+                                mScheme.put(respJsonArray.getJSONObject(i).optString("SN"),
+                                        respJsonArray.getJSONObject(i).optString("ID"));
+                                SchemeList.add(respJsonArray.getJSONObject(i).optString("SN"));
                             }
                             // Spinner adapter
                             spnSchemes.setAdapter(new ArrayAdapter<String>(ProgressMPR.this,
                                     android.R.layout.simple_spinner_dropdown_item,
-                                    groupList));
+                                    SchemeList));
                         } catch (JSONException e) {
                             e.printStackTrace();
                             return;
@@ -198,6 +201,139 @@ public class ProgressMPR extends ActionBarActivity {
         // Adding request to request queue
         jsonObjReq.setTag(TAG);
         rQueue.add(jsonObjReq);
-        Toast.makeText(getApplicationContext(), "Loading All Schemes Please Wait...", Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(), "Loading All Schemes Please Wait...", Toast.LENGTH_SHORT).show();
     }
+
+    private class SchemeSelectionListener implements AdapterView.OnItemSelectedListener{
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            String txtScheme = spnSchemes.getSelectedItem().toString();
+            getUserWorks(txtScheme);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    }
+
+    private void getUserWorks(String SID) {
+
+        final JSONObject jsonPost = new JSONObject();
+
+        //Toast.makeText(getApplicationContext(),
+        //        "SchemeID: " + mScheme.get(SID) + " : " + SID,
+        //        Toast.LENGTH_SHORT).show();
+
+        try {
+            jsonPost.put("API", "UW");
+            jsonPost.put("UID", "5"); // TODO Supply Dynamic UserMapID instead of Static
+            jsonPost.put("SID", mScheme.get(SID));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                API_URL, jsonPost,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e(TAG, "UserWorks: " + response.toString());
+                        Toast.makeText(getApplicationContext(), response.optString(DashAIO.KEY_STATUS), Toast.LENGTH_SHORT).show();
+                        try {
+                            respJsonArray = response.getJSONArray("DB");
+                            ArrayList<String> WorkList = new ArrayList<String>();
+
+                            for (int i = 0; i < respJsonArray.length(); i++) {
+                                WorkList.add(respJsonArray.getJSONObject(i).optString("WorkID")
+                                        + ": " + respJsonArray.getJSONObject(i).optString("Work"));
+                            }
+                            // Spinner adapter
+                            spnWorks.setAdapter(new ArrayAdapter<String>(ProgressMPR.this,
+                                    android.R.layout.simple_spinner_dropdown_item,
+                                    WorkList));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            return;
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Log.e(TAG, jsonPost.toString());
+            }
+        }
+        );
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        mUser.hotpCodeGenerationAllowed = true;
+                    }
+                },
+                HOTP_MIN_TIME_INTERVAL_BETWEEN_CODES
+        );
+
+        // Adding request to request queue
+        jsonObjReq.setTag(TAG);
+        rQueue.add(jsonObjReq);
+        //Toast.makeText(getApplicationContext(), "Loading All Schemes Please Wait...", Toast.LENGTH_SHORT).show();
+    }
+
+    private class UpdateClickListener implements View.OnClickListener {
+        private final Handler mHandler = new Handler();
+
+        @Override
+        public void onClick(View view) {
+
+            String txtMsg = etRemarks.getText().toString();
+
+            if (txtMsg.length() > 0) {
+                Toast.makeText(getApplicationContext(), "Message Size: " + txtMsg.length(), Toast.LENGTH_SHORT).show();
+                try {
+                    String oldPin = mUser.pin;
+                    mUser.pin = mOtpProvider.getNextCode(mUser.user);
+                    if (mUser.pin.equals(oldPin) || !mUser.hotpCodeGenerationAllowed) {
+                        Toast.makeText(getApplicationContext(), "Please wait for a while to generate new OTP for"
+                                + " MDN:" + mUser.user, Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                } catch (OtpSourceException e) {
+                    Toast.makeText(getApplicationContext(), "Error: " + e.getMessage()
+                            + " MDN:" + mUser.user, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Temporarily disable code generation for this account
+                mUser.hotpCodeGenerationAllowed = false;
+
+                // The delayed operation below will be invoked once code generation is yet again allowed for
+                // this account. The delay is in wall clock time (monotonically increasing) and is thus not
+                // susceptible to system time jumps.
+                mHandler.postDelayed(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                mUser.hotpCodeGenerationAllowed = true;
+                            }
+                        },
+                        HOTP_MIN_TIME_INTERVAL_BETWEEN_CODES
+                );
+
+                //UpdateProgress();
+
+            } else {
+                Toast.makeText(getApplicationContext(), getString(R.string.msg_warn_remarks), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
 }
